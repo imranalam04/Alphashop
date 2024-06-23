a80a9f892365c36aea50cfe392ffb28107e57f2a
8m4an <34647817+imranalam04@users.noreply.github.com>
2024-06-23 18:08:01 +0530

    "backend-setup"

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,39 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import "../assets/styles/index.css";
+import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
+const Header = () => {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand className="navbar-brand-custom">
+              AlphaShop
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart />
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser />
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
